d7f9307190b718c5690448fc31df0d7db5d73988
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2020-11-29 21:12:54 +0530

    coloured bubble sort

--- sorts.py
+++ sorts.py
@@ -7,19 +7,23 @@ def bubble(data):
 
     for i in range(n):
         for j in range(n-i-1):
+            if(i):
+                data[-i][1] = 'green'
             data[j][1] = 'red'
-            data[j+1][1] = 'red'
+            data[j+1][1] = 'blue'
 
             datalist.append(copy.deepcopy(data))
 
             if (data[j][0] > data[j+1][0]):
                 data[j] , data[j+1] = data[j+1] , data[j]
 
+            datalist.append(copy.deepcopy(data))
+
             data[j][1] = 'white'
             data[j+1][1] = 'white'
 
-    data[0][1] = 'white'
-    data[1][1] = 'white'
+    data[0][1] = 'green'
+    data[1][1] = 'green'
     datalist.append(copy.deepcopy(data))
 
     return datalist
